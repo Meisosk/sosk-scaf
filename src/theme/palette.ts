f4c12bce46d62775d64a2154340b61935ecd00e2
Meir Soskil <meirsoskil@gmail.com>
2026-07-07 18:15:38 -0400

    add footer and fix palette

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,12 +1,38 @@
 export const palette = {
-  primary: {
-    main: "#2563eb",
+  light: {
+    palette: {
+      primary: {
+        main: "#274C77",
+      },
+      secondary: {
+        main: "#6096BA",
+      },
+      background: {
+        default: "#E7ECEF",
+        paper: "#ffffff",
+      },
+      text: {
+        primary: "#274C77",
+        secondary: "#8B8C89",
+      },
+    },
   },
-  secondary: {
-    main: "#f97316",
-  },
-  background: {
-    default: "#f9fafb",
-    paper: "#ffffff",
+  dark: {
+    palette: {
+      primary: {
+        main: "#A3CEF1",
+      },
+      secondary: {
+        main: "#6096BA",
+      },
+      background: {
+        default: "#274C77",
+        paper: "#1f3a5a",
+      },
+      text: {
+        primary: "#E7ECEF",
+        secondary: "#8B8C89",
+      },
+    },
   },
 };
